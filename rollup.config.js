3731ea106edbb65afb940059761ae4a1c754224e
Saul Lee <solleedata@gmail.com>
2025-03-13 14:34:55 +0900

    feat: update build configuration and add tslib dependency

--- rollup.config.js
+++ rollup.config.js
@@ -1,36 +1,15 @@
 import typescript from '@rollup/plugin-typescript';
-import commonjs from '@rollup/plugin-commonjs';
-import resolve from '@rollup/plugin-node-resolve';
-import dts from 'rollup-plugin-dts';
 
-const packageJson = require('./package.json');
-
-export default [
-  {
-    input: 'src/index.ts',
-    output: [
-      {
-        file: packageJson.main,
-        format: 'cjs',
-        sourcemap: true,
-      },
-      {
-        file: packageJson.module,
-        format: 'esm',
-        sourcemap: true,
-      },
-    ],
-    plugins: [
-      resolve(),
-      commonjs(),
-      typescript({ tsconfig: './tsconfig.json' }),
-    ],
-    external: ['react', 'react-dom'],
+export default {
+  input: 'src/index.js',
+  output: {
+    dir: 'dist',
+    format: 'esm',
   },
-  {
-    input: 'dist/esm/types/index.d.ts',
-    output: [{ file: 'dist/index.d.ts', format: 'esm' }],
-    plugins: [dts()],
-    external: [/\.css$/],
-  },
-];
+  plugins: [
+    typescript({
+      tsconfig: './tsconfig.json',
+      outDir: 'dist',
+    }),
+  ],
+};
