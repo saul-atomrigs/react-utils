2398718b1c4726029f839083d33126724a9604f2
Saul Lee <solleedata@gmail.com>
2025-03-19 16:29:02 +0900

    feat: add QueryAsyncBoundary component and update dependencies

---
+++ src/query-async-boundary.tsx
@@ -0,0 +1,33 @@
+import React, { Suspense, type PropsWithChildren } from 'react';
+import { useQueryErrorResetBoundary } from '@tanstack/react-query';
+import { ErrorBoundary } from 'react-error-boundary';
+
+type QueryAsyncBoundaryProps = {
+  pendingFallback: React.ReactNode;
+  rejectedFallback?: React.ReactNode;
+} & PropsWithChildren;
+
+/**
+ * reference: https://github.com/ssi02014/react-query-tutorial?tab=readme-ov-file#usequeryerrorresetboundary
+ * PLEASE use throwOnError: true in the query options to make sure the error is thrown and caught by the error boundary.
+ */
+export const QueryAsyncBoundary = ({
+  pendingFallback,
+  rejectedFallback,
+  children,
+}: QueryAsyncBoundaryProps) => {
+  const { reset } = useQueryErrorResetBoundary();
+
+  return (
+    <ErrorBoundary
+      onReset={reset}
+      fallbackRender={({ resetErrorBoundary }) =>
+        rejectedFallback || (
+          <button onClick={() => resetErrorBoundary()}>Try again</button>
+        )
+      }
+    >
+      <Suspense fallback={pendingFallback}>{children}</Suspense>
+    </ErrorBoundary>
+  );
+};
